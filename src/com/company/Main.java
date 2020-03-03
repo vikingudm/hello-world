651f7c95c3786bfab086675fa1f7fa9134c3d6a9
Vitaliy Mikhaylov <vikingudm@mail.ru>
2020-03-04 00:07:10 +0400

    create hello world

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String hello = "Hello";
+        String world = "world";
+        System.out.println(hello + " " + world + "!");
+    }
+}
